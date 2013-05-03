1fdd84ec2a0d8f796b3ecfdd504bca1573022ffb
rayzeller@gmail.com <rayzeller@gmail.com>
2013-05-02 17:08:45 -0700

    actually evaluate data methods

--- lib/moonshine/fermenter.rb
+++ lib/moonshine/fermenter.rb
@@ -1,11 +1,13 @@
 module Moonshine
   class Fermenter
+    DEFAULT_TIME = :created_at
+
     class_attribute :_data
     class_attribute :_type
     class_attribute :_time
 
     self._data = {}
-    self._time = Time.zone.now
+    self._time = DEFAULT_TIME
 
     class << self
 
@@ -55,6 +57,34 @@ module Moonshine
       @object, @options = object, options
     end
 
+    def type
+      self._type
+    end
+
+    def time
+      begin
+        object.send self._time
+      rescue  NoMethodError
+        Time.zone.now
+      end
+    end
+
+    def data
+      _fast_data
+      rescue NameError
+        method = "def _fast_data\n"
+
+        method << "  h = {}\n"
+
+        _data.each do |name,key|
+          method << "  h[:\"#{key}\"] = send(:\"#{name}\")\n"
+        end
+        method << "  h\nend"
+
+        self.class.class_eval method
+        _fast_data
+    end
+
     def as_json(options = {})
       return nil if @object.nil?
 
@@ -62,9 +92,9 @@ module Moonshine
       self._type ||= class_name
 
       @options[:hash] = hash = {}
-      hash[:data] = self._data
-      hash[:time] = self._time
-      hash[:type] = self._type
+      hash[:data] = data
+      hash[:time] = time
+      hash[:type] = type
       hash
     end
 
