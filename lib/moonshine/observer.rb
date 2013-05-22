e0672745baee766462a83c44eabcfb36dffa3697
rayzeller@gmail.com <rayzeller@gmail.com>
2013-05-22 11:03:48 -0700

    instance methods module take out

--- lib/moonshine/observer.rb
+++ lib/moonshine/observer.rb
@@ -3,16 +3,14 @@ module Moonshine
     require 'delayed_job'
 
     extend ActiveSupport::Concern
+    def ferment
+      Moonshine.send(self.fermenter.new(self).as_json)
+    end
     included do
       after_commit :ferment, :on => :create, :if => lambda {|o| (!Rails.env.test? && !Rails.env.development?)}
       after_commit :ferment_without_delay, :on => :create, :if => lambda {|o| (Rails.env.test? || Rails.env.development?)}
-    end
-    module InstanceMethods
-
-      def ferment
-        Moonshine.send(self.fermenter.new(self).as_json)
-      end
       handle_asynchronously :ferment
-    end 
+    end
+    
   end
 end
